a84386af3721aebe01169a9a46891cb7aa499787
ShashankNeudose <manikons@mcmaster.ca>
2020-03-06 18:26:36 -0500

    Updated ardino code for MUX
    Updated ardino code for MUX

--- sketch_feb26a/sketch_feb26a.ino
+++ sketch_feb26a/sketch_feb26a.ino
@@ -1,5 +1,12 @@
 #include <Wire.h>
- 
+
+void TCA9548A(uint8_t bus)
+{
+  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
+  Wire.write(1 << bus);          // send byte to select bus
+  Wire.endTransmission();
+}
+
 void setup() {
   Wire.begin();
   Serial.begin(115200);
--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -1,39 +1,80 @@
-#include <AsyncDelay.h>
-#include <SoftWire.h>
-
 #include <max86150.h>
 #include <Wire.h>
 #include "max86150.h"
 
-MAX86150 max86150Sensor;
+void TCA9548A(uint8_t bus)
+{
+  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
+  Wire.write(1 << bus);          // send byte to select bus
+  Wire.endTransmission();
+}
+
+
+MAX86150 max86150Sensor1;
+MAX86150 max86150Sensor2;
 
 #define debug Serial //Uncomment this line if you're using an Uno or ESP
 //#define debug SerialUSB //Uncomment this line if you're using a SAMD21
 
-uint16_t ppgunsigned16;
+uint16_t Appgunsigned16;
+
+uint16_t Bppgunsigned16;
 
 void setup()
 {
-    debug.begin(9600);
+    debug.begin(115200);
     debug.println("MAX86150 PPG Streaming Example");
-
+    Wire.begin();
+    
+    TCA9548A(1);    
     // Initialize sensor
-    if (max86150Sensor.begin(Wire, I2C_SPEED_FAST) == false)
+    if (max86150Sensor1.begin(Wire, I2C_SPEED_FAST) == false)
     {
         debug.println("MAX86150 was not found. Please check wiring/power. ");
         while (1);
     }
 
-    debug.println(max86150Sensor.readPartID());
+    debug.println(max86150Sensor1.readPartID());
 
-    max86150Sensor.setup(); //Configure sensor. Use 6.4mA for LED drive
+    max86150Sensor1.setup(); //Configure sensor. Use 6.4mA for LED drive
+
+
+
+
+    TCA9548A(2);    
+    // Initialize sensor
+    if (max86150Sensor2.begin(Wire, I2C_SPEED_FAST) == false)
+    {
+        debug.println("MAX86150 was not found. Please check wiring/power. ");
+        while (1);
+    }
+
+    debug.println(max86150Sensor2.readPartID());
+
+    max86150Sensor2.setup(); //Configure sensor. Use 6.4mA for LED drive
+
+    TCA9548A(1);
+    TCA9548A(2);
 }
 
 void loop()
 {
-    if(max86150Sensor.check()>0)
+  TCA9548A(1);
+  
+    if(max86150Sensor1.check()>0)
     {
-        ppgunsigned16 = (uint16_t) (max86150Sensor.getFIFORed()>>2);
-        debug.println(ppgunsigned16);
+        Appgunsigned16 = (uint16_t) (max86150Sensor1.getFIFORed()>>2);
+        debug.println(Appgunsigned16);
     }
+    
+
+  TCA9548A(2);
+  
+    if(max86150Sensor2.check()>0)
+    {
+        Bppgunsigned16 = (uint16_t) (max86150Sensor2.getFIFORed()>>2);
+        debug.println(Bppgunsigned16);
+    }
+
+delay(500);
 }
